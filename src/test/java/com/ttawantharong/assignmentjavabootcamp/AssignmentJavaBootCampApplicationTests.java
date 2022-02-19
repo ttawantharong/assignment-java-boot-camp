4cf70b2a796c8a8aad44f8f88d5e4d0c244d587f
ttawantharong <t.tawantharong@gmail.com>
2022-02-19 18:03:56 +0700

    Refs: #1
    feat: initial project

---
+++ src/test/java/com/ttawantharong/assignmentjavabootcamp/AssignmentJavaBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttawantharong.assignmentjavabootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJavaBootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
